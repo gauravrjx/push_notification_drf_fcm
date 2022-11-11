6cbb3290a5a1dbad8bafe1cc9b277a873d41d0fc
gaurav <gaurav.jaiswal@rojgari.com>
2022-11-11 17:54:56 +0545

    added lsdnfofin

---
+++ fcm_manager.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+
+def send_push(title, msg, registration_token, dataObject=None):
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification(
+            title=title,
+            body=msg
+        ),
+        data=dataObject,
+        tokens=registration_token
+    )
+
+
+    response=messaging.send_multicast(message)
+
+    print("successfully sent: ", response)
